2a9e3e8fdd5b3d697f61c33286ab2a66912a375a
Antonio Fernando <antonio.fernando@mail.com>
2022-10-19 00:14:52 -0300

    flask-login

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+login_manager = LoginManager()
 db = SQLAlchemy()
 DB_NAME = "db.sqlite3"
-
 app = Flask(__name__)
+login_manager.init_app(app)
 
 app.config['SECRET_KEY'] = 'derfgtyhjuik'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
 db.init_app(app)
+
+from .models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
--- src/view.py
+++ src/view.py
@@ -25,3 +25,17 @@ class LoginView:
             'login.jinja2.html',
             pid = self.pid
         )
+
+	def login2(self):
+		form = LoginForm()
+		if form.validate_on_submit():
+			login_user(user)
+
+			flask.flash('Logged in successfully.')
+
+			next = flask.request.args.get('next')
+			if not is_safe_url(next):
+				return flask.abort(400)
+
+			return flask.redirect(next or flask.url_for('index'))
+		return flask.render_template('login.html', form=form)
