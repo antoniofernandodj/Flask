2afeaf9722b3bbf5e50c1d7527840d33c61d270f
Antonio Fernando <antonio.fernando@mail.com>
2022-12-19 02:07:01 -0300

    type hint

--- src/libraries/routing.py
+++ src/libraries/routing.py
@@ -1,8 +1,9 @@
 from typing import Dict, Union, List, Optional
 from flask.views import View
 
+
 def path(
-    route: str, view_function: View, methods: Optional[List[str]] = None
-) -> Dict[str, Union[str, View, List[str]]]:
+    route: str, view_function: callable, methods: Optional[List[str]] = None
+) -> Dict[str, Union[str, callable, List[str]]]:
     """Retorna um objeto usado para cadastro de rotas da aplicação Flask"""
     return {'route': route, 'func': view_function, 'methods': methods}
